6039bcd6d1288a5647256692f87195cf89d26618
AstanSimaga <asima104@uottawa.ca>
2020-11-26 02:05:31 -0500

    Found and changed error in ProductTest.java

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -20,7 +20,8 @@ public class ProductTest {
     public void checkProductPrice() {
         // I am making this test to fail
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", 180, aProduct.getPrice());
+        double expectedDouble = 180;
+        assertEquals("Check the price of the product", expectedDouble, aProduct.getPrice());
     }
 
 
